fc949fa24d3a16ad2f6f52a2668150b27cda7a91
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-11-09 23:15:00 -0500

    Instances

---
+++ Objects/src/main/java/com/instances/examples/Student.java
@@ -0,0 +1,37 @@
+package com.instances.examples;
+public class Student
+{
+    private String firstName;
+    private String lastName;
+    private int gradeLevel;
+    private double gpa;
+    // Add GPA instance variable called gpa here.
+    
+    /**
+     * This is a constructor.  A constructor is a method
+     * that creates an object -- it creates an instance
+     * of the class. What that means is it takes the input
+     * parameters and sets the instance variables (or fields)
+     * to the proper values.
+     * 
+     * Check out StudentTester.java for an example of how to use
+     * this constructor and how to add the gpa to the constructor. 
+     */
+    public Student(String fName, String lName, int grade, double sGpa)
+    {
+        firstName = fName;
+        lastName = lName;
+        gradeLevel = grade;
+        gpa=sGpa;
+    }
+    
+    /**
+     * This is a toString for the Student class. It returns a String
+     * representation of the object, which includes the fields
+     * in that object.
+     */
+    public String toString()
+    {
+        return firstName + " " + lastName + " is in grade: " + gradeLevel + " and has GPA: " + gpa;
+    }
+}

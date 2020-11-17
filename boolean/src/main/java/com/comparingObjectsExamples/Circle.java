fb8af55bb2cfb9b5e7eacd6bc5da2309dc73cc8f
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-11-17 09:37:08 -0500

    Comparing objects

---
+++ boolean/src/main/java/com/comparingObjectsExamples/Circle.java
@@ -0,0 +1,49 @@
+package com.comparingObjectsExamples;
+public class Circle
+{
+    private int radius;
+    private String color;
+    private int x;
+    private int y;
+    
+    public Circle(int theRadius, String theColor, int xPosition, int yPosition)
+    {
+        radius = theRadius;
+        color = theColor;
+        x = xPosition;
+        y = yPosition;
+    }
+    
+    public int getRadius()
+    {
+        return radius;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+    
+    public String getColor()
+    {
+        return color;
+    }
+    
+    // Implement a toString method and an equals method here!
+    public String toString()
+    {
+        // Change this!
+        return color+ " circle with a radius of " +radius+ " at position (" +x+ ", " +y+ ")";
+    }
+    
+    public boolean equals(Circle other)
+    {
+        boolean equals=(radius==other.radius) && (color.equals(other.color)) && (x==other.x) && (y==other.y);
+        return equals;
+    }
+}

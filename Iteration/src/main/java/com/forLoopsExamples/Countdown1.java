508eca0e228065e986173eb8a7e911103b9cd51d
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-11-21 16:53:57 -0500

    Same program with for & while loops

---
+++ Iteration/src/main/java/com/forLoopsExamples/Countdown1.java
@@ -0,0 +1,12 @@
+package com.forLoopsExamples;
+public class Countdown1
+{
+    public static void main(String[] args)
+    {
+        //using for loop
+       for(int x = 10; x > 0; x--)
+        {
+            System.out.println(x);
+        }
+    }
+}
---
+++ Iteration/src/main/java/com/forLoopsExamples/Countdown2.java
@@ -0,0 +1,15 @@
+package com.forLoopsExamples;
+public class Countdown2
+{
+    public static void main(String[] args)
+    {
+        // using while loops
+    	int x=10;
+    	while (x > 0)
+    	{
+    		System.out.println(x);
+    		x--;
+    	}
+
+    }
+}

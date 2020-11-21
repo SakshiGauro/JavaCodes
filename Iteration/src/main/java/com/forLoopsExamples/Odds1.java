3e9a53780b1c695408c138a70a41c0094a8b3d8c
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-11-21 17:03:04 -0500

    Equivalent loops (odds 1 to 10)

---
+++ Iteration/src/main/java/com/forLoopsExamples/Odds1.java
@@ -0,0 +1,14 @@
+package com.forLoopsExamples;
+public class Odds1
+{
+    public static void main(String[] args)
+    {
+        //equivalent while loop.
+        int x = 1;
+        while (x <= 10)
+        {
+            System.out.println(x);
+            x +=2;
+        }
+    }
+}
---
+++ Iteration/src/main/java/com/forLoopsExamples/Odds2.java
@@ -0,0 +1,13 @@
+package com.forLoopsExamples;
+
+public class Odds2 
+{
+	    public static void main(String[] args)
+	    {
+	        // equivalent for loop
+	        for (int x = 1; x <= 10; x +=2)
+	        {
+	            System.out.println(x);
+	        }
+	    }
+}

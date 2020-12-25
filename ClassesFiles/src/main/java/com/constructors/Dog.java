bf1249dfddfbcfe1c8afd91b813653c83c812d53
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-25 17:08:56 -0500

    dog class

---
+++ ClassesFiles/src/main/java/com/constructors/Dog.java
@@ -0,0 +1,26 @@
+package com.constructors;
+public class Dog
+{
+    private String name;
+    private int age;
+    private double weight;
+
+    // Add your constructors here
+    public Dog(String theName, int theAge, double theWeight)
+    {
+        name=theName;
+        age=theAge;
+        weight=theWeight;
+    }
+    
+    public Dog(String theName, int theAge)
+    {
+        name=theName;
+        age=theAge;
+        weight=0.0;
+    }
+
+    public String toString(){
+        return "Name: " + name + "\nWeight: " + weight +  "\nAge: " + age;
+    }
+}
---
+++ ClassesFiles/src/main/java/com/constructors/DogTester.java
@@ -0,0 +1,13 @@
+package com.constructors;
+public class DogTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Dog Karel= new Dog("Karel", 7,15.6);
+        Dog Clover= new Dog("Clover", 9);
+        
+        System.out.println(Karel);
+        System.out.println(Clover);
+    }
+}

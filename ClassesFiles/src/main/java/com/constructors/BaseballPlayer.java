395a75336a1e1b898475eebe4d685f520669a4ac
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-25 17:06:37 -0500

    Baseball

---
+++ ClassesFiles/src/main/java/com/constructors/BaseballPlayer.java
@@ -0,0 +1,28 @@
+package com.constructors;
+public class BaseballPlayer
+{
+    private int hits;
+    private int atBats;
+    private String name;
+    
+    //Add constructor here
+    public BaseballPlayer(String nName, int hHits, int aAtBats)
+    {
+        name=nName;
+        hits=hHits;
+        atBats=aAtBats;
+    }
+    
+    public void printBattingAverage()
+    {
+        double battingAverage = hits / (double) atBats;
+        System.out.println(battingAverage);
+    }
+    
+    public String toString()
+    {
+        return name + ": " + hits + "/" + atBats;
+    }
+    
+    
+}
---
+++ ClassesFiles/src/main/java/com/constructors/BaseballTester.java
@@ -0,0 +1,13 @@
+package com.constructors;
+public class BaseballTester
+{
+    public static void main(String[] args)
+    {
+        BaseballPlayer babeRuth = new BaseballPlayer("Babe Ruth", 2873, 8399);
+        System.out.println(babeRuth);
+        
+        // Call the function printBattingAverage here
+        babeRuth.printBattingAverage();
+        
+    }
+}

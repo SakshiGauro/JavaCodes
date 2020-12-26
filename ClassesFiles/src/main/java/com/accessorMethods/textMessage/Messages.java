be435c9961fd4cb852f65ffb9bd661ea3057c1ca
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-25 20:15:00 -0500

    Text Messages Getter Methods

---
+++ ClassesFiles/src/main/java/com/accessorMethods/textMessage/Messages.java
@@ -0,0 +1,14 @@
+package com.accessorMethods.textMessage;
+
+public class Messages
+{
+    public static void main(String[] args)
+    {
+        // Your code here.
+        // Create two TextMessage objects and print them out.
+        TextMessage text1 = new TextMessage("Sandra","Sakshi","Hi");
+        TextMessage text2 = new TextMessage("Sakshi","Sandra","Hello");
+        System.out.println(text1);
+        System.out.println(text2);
+    }
+}
---
+++ ClassesFiles/src/main/java/com/accessorMethods/textMessage/textMessage.md
@@ -0,0 +1,9 @@
+# Text Message
+The `TextMessage` class was created to save text messages. Currently, the class has a constructor and a `toString`.
+
+Continue adding to this class by adding three accessor methods:
+```
+public String getSender()
+public String getReceiver()
+public String getMessage()
+```

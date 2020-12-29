4d42e0c0c23ba463f8bf8762c24cc11c6ae04697
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-29 14:33:14 -0500

    Full Fraction Class

--- ClassesFiles/src/main/java/com/mutatorsMethods/fraction/FractionTester.java
+++ ClassesFiles/src/main/java/com/mutatorsMethods/fraction/FractionTester.java
@@ -1,26 +1,30 @@
 package com.mutatorsMethods.fraction;
 
-public class FractionTester
+public class FractionTester extends Object
 {
     public static void main(String[] args)
     {
         // Implement the Fraction class in Fraction.java
         // Then add code here to test out your Fraction class!
-        Fraction frac1= new Fraction(2,3);
-        Fraction frac2 = new Fraction (3,5);
-        
-        Fraction first = new Fraction(1, 2);
-        Fraction second = new Fraction(1, 3);
+        System.out.println("Fraction Method is Called=========>");
+        int a=2;
+        int b=3;
+        System.out.println(2+3);
+        FractionMM frac1= new FractionMM(2,3);
+        FractionMM frac2 = new FractionMM (3,5);
+
+        FractionMM first = new FractionMM(1, 2);
+        FractionMM second = new FractionMM(1, 3);
         System.out.println();
-        
+
         System.out.println("BEFORE:");
         System.out.println("first: " + first);
         System.out.println("second: " + second);
-        
+
         first.multiply(second);
-        
+
         System.out.println("AFTER:");
-        
+
         System.out.println("first: " + first);
         System.out.println("second: " + second);
     }

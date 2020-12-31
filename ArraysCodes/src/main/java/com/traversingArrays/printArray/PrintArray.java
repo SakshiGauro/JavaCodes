8019d8a7773b835c0f8312c680eb510965da3383
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-30 22:49:02 -0500

    Print Array

---
+++ ArraysCodes/src/main/java/com/traversingArrays/printArray/PrintArray.java
@@ -0,0 +1,19 @@
+package com.traversingArrays.printArray;
+
+public class PrintArray
+{
+    public static void main(String[] args)
+    {
+        String[] arr = new String[]{"a", "b", "c"};
+        printArr(arr);
+    }
+    
+    public static void printArr(String[] arr)
+    {
+        // Print everything in the array on its own line
+        for (int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
---
+++ ArraysCodes/src/main/java/com/traversingArrays/printArray/ReadMe.md
@@ -0,0 +1,2 @@
+# Print Array
+Write a method that prints the contents of an array. Test this method out on the array created for you in the main method.

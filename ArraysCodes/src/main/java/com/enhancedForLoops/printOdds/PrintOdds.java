66fd5c09c3768f363e7933101ffe201863a5f6aa
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-01-02 13:25:56 -0500

    Print Odds

---
+++ ArraysCodes/src/main/java/com/enhancedForLoops/printOdds/PrintOdds.java
@@ -0,0 +1,15 @@
+package com.enhancedForLoops.printOdds;
+
+public class PrintOdds
+{
+   public static void main(String[] args)
+   {
+     int[ ] values = {17, 34, 56, 2, 19, 100};
+        
+     for (int value : values)
+     {
+       if (value % 2 == 1)
+           System.out.println(value + " is odd");
+     }
+   }
+}
---
+++ ArraysCodes/src/main/java/com/enhancedForLoops/printOdds/ReadMe.md
@@ -0,0 +1,2 @@
+# Print Odds
+In this exercise, rewrite the `for` loop in the starter code, which prints out the odd numbers in an array, as an enhanced `for-each` loop.

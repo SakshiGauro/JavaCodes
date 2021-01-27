e145a415ec73d8fb27dde980298b839fec966108
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-01-26 19:43:40 -0500

    Road Trip!

--- ArrayList/src/main/java/com/traversing/roadTrip/GeoLocation.java
+++ ArrayList/src/main/java/com/traversing/roadTrip/GeoLocation.java
@@ -3,14 +3,13 @@ package com.traversing.roadTrip;/*
  * specified using latitude and longitude.  The class includes a method for
  * computing the distance between two locations.
  *
- * This implementation is based off of the example from Stuart Reges at 
+ * This implementation is based off of the example from Stuart Reges at
  * the University of Washington.
  */
 
-public class GeoLocation 
-{
+public class GeoLocation {
     // Earth radius in miles
-    public static final double RADIUS = 3963.1676;  
+    public static final double RADIUS = 3963.1676;
 
     private double latitude;
     private double longitude;
@@ -19,8 +18,7 @@ public class GeoLocation
     /**
      * Constructs a geo location object with given latitude and longitude
      */
-    public GeoLocation(String name,double theLatitude, double theLongitude ) 
-    {
+    public GeoLocation(String name, double theLatitude, double theLongitude) {
         latitude = theLatitude;
         longitude = theLongitude;
         this.name = name;
@@ -29,34 +27,29 @@ public class GeoLocation
     /**
      * Returns the latitude of this geo location
      */
-    public double getLatitude() 
-    {
+    public double getLatitude() {
         return latitude;
     }
 
     /**
      * returns the longitude of this geo location
      */
-    public double getLongitude() 
-    {
+    public double getLongitude() {
         return longitude;
     }
 
     // returns a string representation of this geo location
-    public String toString() 
-    {
+    public String toString() {
         return name + " (" + latitude + ", " + longitude + ")";
     }
-    
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
 
     // returns the distance in miles between this geo location and the given
     // other geo location
-    public double distanceFrom(GeoLocation other) 
-    {
+    public double distanceFrom(GeoLocation other) {
         double lat1 = Math.toRadians(latitude);
         double long1 = Math.toRadians(longitude);
         double lat2 = Math.toRadians(other.latitude);
@@ -64,7 +57,7 @@ public class GeoLocation
         // apply the spherical law of cosines with a triangle composed of the
         // two locations and the north pole
         double theCos = Math.sin(lat1) * Math.sin(lat2) +
-            Math.cos(lat1) * Math.cos(lat2) * Math.cos(long1 - long2);
+                Math.cos(lat1) * Math.cos(lat2) * Math.cos(long1 - long2);
         double arcLength = Math.acos(theCos);
         return arcLength * RADIUS;
     }
